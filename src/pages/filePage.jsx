f07f640983a5b2a7235af3c02563923af20b3fa3
stchshin <stchshin@gmail.com>
2026-08-06 11:06:46 +0900

    feat:파일 업로드 및 다운로드

---
+++ src/pages/filePage.jsx
@@ -0,0 +1,70 @@
+import axiosInstance from "./api/axiosInstance";
+import { useState } from 'react';
+
+export default function filePage() {
+  const [file, setFile] = useState(null);
+  const [uploadUrl, setUploadUrl] = useState(null);
+  const [key, setKey] = useState('');
+  const [downloadUrl, setDownloadUrl] = useState('');
+
+  function handleFileChange(e) {
+    setFile(e.target.files[0]);
+  }
+
+  async function handleUpload() {
+    // Presigned url
+    try {
+      const response = await axiosInstance.post(
+        '/files/presigned-upload',
+        {
+          "fileName": file.name,
+          "contentType": 'model/stl'
+        }
+      )
+      setUploadUrl(response.data.uploadUrl);
+      setKey(response.data.key);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Upload
+    try {
+      const response = await axiosInstance.put(
+        uploadUrl,
+        file,
+        {
+          headers: {
+            'Content-Type': 'model/stl'
+          }
+        }
+      )
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleDownload() {
+    try {
+      const response = await axiosInstance.get(
+        '/files/download-url',
+        { params: {
+            'key': key
+          }
+        }
+      )
+      setDownloadUrl(response.data.fileUrl);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+  return (
+    <>
+      <input onChange={handleFileChange} type="file" />
+      <button onClick={handleUpload}>Upload</button>
+      <button onClick={handleDownload}>Download</button>
+      <a href={downloadUrl}>Click me!</a>
+    </>
+  )
+}
