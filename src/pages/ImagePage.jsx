bdc3102bf23c5a7eaab3688b752d374b807421d2
stchshin <stchshin@gmail.com>
2026-08-16 12:41:35 +0900

    fix: 병합 후 자잘한 버그 수정

--- src/pages/ImagePage.jsx
+++ src/pages/ImagePage.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router';
+import { useLocation, useNavigate, useParams } from 'react-router';
 import axiosInstance from "../api/axiosInstance";
 import Loading from "../components/Loading/Loading";
 import { PageContext } from "../layouts/PageLayout";
@@ -8,7 +8,8 @@ import { KeyContext } from "../App";
 export default function ImagePage() {
   // 버튼 레이아웃 관련 부분 - 설명은 layouts/PageLayout.jsx 참고
   const { setNextButtonText, setNextButtonActive, setNextButtonOnclick } = useContext(PageContext);
-  const { key, creationId, setStylizedImageUrl } = useContext(KeyContext);
+  const { key, setStylizedImageUrl } = useContext(KeyContext);
+  const { creationId } = useParams();
   const navigate = useNavigate();
   const location = useLocation();
   const prompt = location.state?.prompt;
